7e1bf074329af9d86dc5a5e3b845196cb5665c17
JG00568405 <JG00568405@INBASDPC09646.TechMahindra.com>
2018-11-09 15:02:08 +0530

    my commit 1

---
+++ Demo1/src/main/java/com/maventest/Demo1/App.java
@@ -0,0 +1,28 @@
+package com.maventest.Demo1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+        System.out.println("hi maven");
+        System.out.println("hi maven");
+        System.out.println("hi mave3");
+        System.out.println("hi mave3");
+        System.out.println("hi mave3");
+        System.out.println("hi mave3");
+        System.out.println("hi mave3");
+
+
+        
+        
+        
+        
+        
+    }
+}
